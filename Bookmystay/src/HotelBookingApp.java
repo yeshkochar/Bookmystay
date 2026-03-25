2dc7f5a9492e6d3dc11d840e606031a32e82ee89
yeshkochar <kocharyesh@yahoo.com>
2026-03-25 15:49:15 +0530

    task descriptionUC5

--- Bookmystay/src/HotelBookingApp.java
+++ Bookmystay/src/HotelBookingApp.java
@@ -1,30 +1,27 @@
 import java.util.*;
-
-abstract class Rm {
-    String t; int b; double p;
-    Rm(String t, int b, double p) { this.t = t; this.b = b; this.p = p; }
-    void disp() { System.out.println(t + " | " + b + " Bed | $" + p); }
+class Res {
+    String n, t;
+    Res(String n, String t) { this.n = n; this.t = t; }
+    void p() { System.out.println(n + " -> " + t); }
 }
-class Sgl extends Rm { Sgl() { super("Single", 1, 100); } }
-class Dbl extends Rm { Dbl() { super("Double", 2, 180); } }
-class Ste extends Rm { Ste() { super("Suite", 3, 350); } }
-class Inv {
-    Map<String, Integer> m = new HashMap<>();
-    Inv() { m.put("Single", 5); m.put("Double", 0); m.put("Suite", 2); }
-}
-class Srch {
-    void run(Inv i, List<Rm> list) {
-        System.out.println("Available Rooms");
-        list.stream().filter(r -> i.m.getOrDefault(r.t, 0) > 0).forEach(r -> {
-            r.disp();
-            System.out.println("Stock: " + i.m.get(r.t) + "\n");
-        });
+class BookQ {
+    Queue<Res> q = new LinkedList<>();
+    void add(Res r) {
+        q.add(r);
+        System.out.println("Queued: " + r.n);
+    }
+    void list() {
+        System.out.println("\n--- FIFO Queue ---");
+        q.forEach(Res::p);
+        System.out.println("------------------\n");
     }
 }
-public class Main {
+public class UseCase5BookingRequestQueue {
     public static void main(String[] args) {
-        Inv i = new Inv();
-        List<Rm> catalog = Arrays.asList(new Sgl(), new Dbl(), new Ste());
-        new Srch().run(i, catalog);
+        BookQ bq = new BookQ();
+        bq.add(new Res("Alice", "Suite"));
+        bq.add(new Res("Bob", "Single"));
+        bq.add(new Res("Charlie", "Double"));
+        bq.list();
     }
 }
